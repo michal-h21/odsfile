f53ed0f32b7da2161ed757733566a316f92a34e6
Michal Hoftich <michal.h21@gmail.com>
2015-09-20 16:40:58 +0200

    Fixed text:s issue. Hopefully

--- odsfile.lua
+++ odsfile.lua
@@ -212,6 +212,30 @@ function escape(s)
   return string.gsub(s, "([#%%$&])", "\\%1")
 end
 
+function print_r ( t ) 
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    sub_print_r(t,"  ")
+end
 
 function get_cell(val, delim)
   local val = val or ""
@@ -224,7 +248,8 @@ function get_cell(val, delim)
     elseif val["text:span"] then
       return get_cell(val["text:span"], delim)
     elseif val["text:s"] then
-      return get_cell(val["text:s"], delim)
+      -- return get_cell(val["text:s"], delim)
+      return table.concat(val, " ")
     else
       local t = {}
       for _,v in ipairs(val) do
