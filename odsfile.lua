adede2e95b84e1cd280da6802768352b71a27326
Michal Hoftich <michal.h21@gmail.com>
2015-09-20 16:41:34 +0200

    Removed print_r helper

--- odsfile.lua
+++ odsfile.lua
@@ -212,31 +212,6 @@ function escape(s)
   return string.gsub(s, "([#%%$&])", "\\%1")
 end
 
-function print_r ( t ) 
-    local print_r_cache={}
-    local function sub_print_r(t,indent)
-        if (print_r_cache[tostring(t)]) then
-            print(indent.."*"..tostring(t))
-        else
-            print_r_cache[tostring(t)]=true
-            if (type(t)=="table") then
-                for pos,val in pairs(t) do
-                    if (type(val)=="table") then
-                        print(indent.."["..pos.."] => "..tostring(t).." {")
-                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
-                        print(indent..string.rep(" ",string.len(pos)+6).."}")
-                    else
-                        print(indent.."["..pos.."] => "..tostring(val))
-                    end
-                end
-            else
-                print(indent..tostring(t))
-            end
-        end
-    end
-    sub_print_r(t,"  ")
-end
-
 function get_cell(val, delim)
   local val = val or ""
   local typ = type(val)
